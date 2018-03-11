34a409b41ff2f356a9b7be3ca72525cb49a828a9
bching28 <bryanching7373@gmail.com>
2018-03-11 01:20:07 -0800

    Update logistic_regression_bryan.py

--- logistic_regression_bryan.py
+++ logistic_regression_bryan.py
@@ -1,4 +1,5 @@
 from sklearn import linear_model
+from sklearn import ensemble
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import mean_absolute_error
 import numpy as np
@@ -26,24 +27,24 @@ def createFiles():
     global test_y
 
     train_data = np.load('ecs171train.npy') #data[0,] is the column headers
-    #test_data = np.load('ecs171test.npy')
+    test_data = np.load('ecs171test.npy')
     size_train = train_data.size # 50001
-    #size_test = test_data.size
+    size_test = test_data.size
     train_y = []
     #test_y = []
     id_values_train = []
-    #id_values_test = []
+    id_values_test = []
 
     #train_x = np.empty((0,770), int)
     #test_x = np.empty((0,770), int)
 
     # open the files
     train_x_file = open("train_x.dat","a")
-    #test_x_file = open("test_x.dat","a")
+    test_x_file = open("test_x.dat","a")
     train_y_file = open("train_y.dat","a")
     #test_y_file = open("test_y.dat","a")
     train_id_file = open("train_id.dat","a")
-    #test_id_file = open("test_id.dat","a")
+    test_id_file = open("test_id.dat","a")
 
     # the feature set
 
@@ -60,38 +61,33 @@ def createFiles():
         for item in train_x_list:
             train_x_file.write("%s " % item)
         train_x_file.write("\n")
-        #train_x = np.append(train_x, np.array([train_x_list]), axis=0)
 
     for item in id_values_train:
         train_id_file.write("%s " % item)
         train_id_file.write("\n")
 
-    '''
+
     for j in range(1,size_test):
         print j
-
         test_x_list = test_data[j-1,].split(",") # i-1 b/c test set doesn't have column headers
         #test_x_list = test_x_list[:-1]
-
         id_val = test_x_list.pop(0)
         id_values_test.append(int(id_val))
-
         test_x_list = [float(b) if b != 'NA' else 0 for b in test_x_list]
         for item in test_x_list:
             test_x_file.write("%s " % item)
         test_x_file.write("\n")
         #test_x = np.append(test_x, np.array([test_x_list]), axis=0)
-
     for item in id_values_test:
         test_id_file.write("%s " % item)
         test_id_file.write("\n")
-    '''
+
 
     # close the files
     train_x_file.close()
-    #test_x_file.close()
+    test_x_file.close()
     train_id_file.close()
-    #test_id_file.close()
+    test_id_file.close()
 
     print "Finished feature set"
 
@@ -108,7 +104,6 @@ def createFiles():
     for j in range(1,size_test):
         test_y_list = test_data[i-1,].split(",") # i-1 b/c test set doesn't have column headers
         test_y.append(int(test_y_list[769])) # no loss col?
-
     for item in test_y: #DO WE EVEN HAVE A TEST SET LOSS COL?
         test_y_file.write("%s " % item)
         test_y_file.write("\n")
@@ -125,9 +120,15 @@ def createFiles():
 # load the data as matrices
 train_x = np.loadtxt('train_x.dat')
 train_y = np.loadtxt('train_y.dat')
-#test_x = np.loadtxt('test_x.dat')
+test_x = np.loadtxt('test_x.dat')
 #test_y = np.loadtxt('test_y.dat')
-
+'''
+train_x[:,:-1] = np.subtract(train_x[:,522], train_x[:,521])
+train_x[:,:-1] = np.divide(np.subtract(train_x[:,272], train_x[:,522]), (np.subtract(train_x[:,522], train_x[:,521])+1))
+train_x[:,:-1] = np.divide(train_x[:,269], (np.subtract(train_x[:,522], train_x[:,521])+1))
+print train_x.shape
+time.sleep(10)
+'''
 #training
 sub_train_x = train_x[0:40000,]
 sub_test_x = train_x[40000:50000,]
@@ -136,10 +137,12 @@ sub_test_y = train_y[40000:50000,]
 
 print train_x.shape
 print train_y.shape
-#print test_x.shape
+print test_x.shape
 
-classifier = linear_model.LogisticRegression(solver='saga', max_iter=750, tol=0.003)
+n_estimators = 500
+#classifier = linear_model.LogisticRegression(solver='saga', max_iter=750, tol=0.003)
 #classifier = linear_model.SGDClassifier(loss='log', max_iter=1000)
+classifier = ensemble.GradientBoostingClassifier(n_estimators=n_estimators,random_state=0)
 
 t0 = time()
 #train_accuracy = classifier.fit(sub_train_x, sub_train_y).score(sub_test_x, sub_test_y)
@@ -147,13 +150,16 @@ classifier.fit(sub_train_x, sub_train_y)
 print ("training time:", round(time()-t0, 3), "s")
 #print ("Training Accuracy", train_accuracy)
 
-t1 = time()
-prediction = classifier.predict(sub_test_x)
-print ("testing time:", round(time()-t1, 3), "s")
+#t1 = time()
+#prediction = classifier.predict(sub_test_x)
+#print ("testing time:", round(time()-t1, 3), "s")
 
-print "Accuracy:", accuracy_score(sub_test_y, prediction)
-print "MAE:", mean_absolute_error(sub_test_y, prediction)
+#print "Accuracy:", accuracy_score(sub_test_y, prediction)
+#print "MAE:", mean_absolute_error(sub_test_y, prediction)
 #print ("Prediction:", prediction)
+classifier.score(sub_test_x, sub_test_y) # accuracy of training validation
+prediction = classifier.predict(test_x)
+
 prediction_file = open("prediction.dat","a")
 for item in prediction:
     prediction_file.write("%s " % item)
