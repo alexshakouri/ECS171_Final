c1c9cc7dbd2d03b777ab5f4ab3ad8dcc14afa4bb
Filipe Eduardo Borges <33043322+filipeeduardo@users.noreply.github.com>
2018-03-11 22:21:54 -0700

    Add files via upload

--- scilog.py
+++ scilog.py
@@ -11,35 +11,49 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import mean_absolute_error
 from sklearn.ensemble import GradientBoostingRegressor
-
+from sklearn.model_selection import cross_val_predict
 def loadDat():
-	train = pd.read_csv('train_v2.csv')
+	train = pd.read_csv('data_train.csv')
 	test  = pd.read_csv('data_test.csv')
-	featselect=['f2','f528_diff_f274','f332','f67','f25','f120','f766','f376','f39','f670','f228','f652','f415','f596','f406','f13','f355','f777','f2','f527_diff_f528','f528_diff_f274','f222','f332','f67','f25','f120','f766','f376','f670','f228','f652','f761','f4','f13','f386','f596','f9','f355','f406','f518','f328','f696','f674','f718'];
-	featselect=list(set(featselect));	
+	#featselect=['f2','f528_diff_f274','f332','f67','f25','f120','f766','f376','f39','f670','f228','f652','f415','f596','f406','f13','f355','f777','f2','f527_diff_f528','f528_diff_f274','f222','f332','f67','f25','f120','f766','f376','f670','f228','f652','f761','f4','f13','f386','f596','f9','f355','f406','f518','f328','f696','f674','f718'];
+	#featselect=list(set(featselect));	
 	train['f527_diff_f528'] = train.f527 - train.f528
 	train['f528_diff_f274'] = train.f528 - train.f274
 	test['f527_diff_f528'] = train.f527 - train.f528
 	test['f528_diff_f274'] = train.f528 - train.f274
 	
-	train_X = train[featselect]
+	train_X = train#[featselect]
 	train_Y = train.loss
-	test_X  = test[featselect]
-	
-	for x in featselect:
+
+	test_X  = train[35000:49999]#[featselect]
+	test_Y  = train.loss[35000:49999]
+	del train_X['loss']
+	del train_X['id']
+	del test_X['loss']
+	del test_X['id']
+	realtest_X=test;
+	for x in train_X:
 		if train_X[x].dtype not in ['float64']:
 			train_X[x] = train_X[x].astype(float)
+		if realtest_X[x].dtype not in ['float64']:
+			realtest_X[x] = realtest_X[x].astype(float)
 		if test_X[x].dtype != train_X[x].dtype:
 			test_X[x] = test_X[x].astype(train_X[x].dtype)
+		if realtest_X[x].dtype != train_X[x].dtype:
+			realtest_X[x] = realtest_X[x].astype(train_X[x].dtype)
 		if not all(train_X[x].notnull()):
 			train_X[x] = train_X[x].fillna(train_X[x].median())
 		if not all(test_X[x].notnull()):
 			test_X[x] = test_X[x].fillna(train_X[x].median())
-	idf = test.id
+		if not all(realtest_X[x].notnull()):
+			realtest_X[x] = realtest_X[x].fillna(train_X[x].median())
+	idf = realtest_X.id
+	del realtest_X['id']
 	scale = StandardScaler()
 	train_X = pd.DataFrame(scale.fit_transform(train_X))
 	test_X = pd.DataFrame(scale.fit_transform(test_X))
-	return train_X, train_Y, test_X, idf
+	realtest_X = pd.DataFrame(scale.fit_transform(realtest_X))
+	return train_X, train_Y, test_X, test_Y, realtest_X,idf
 
 def train_model(train_X,train_Y):
 #	indicepo = train_Y>0
@@ -52,22 +66,22 @@ def train_model(train_X,train_Y):
 	classifier.fit(train_X, train_Y)
 	return classifier
 
-def predictor(classifier,test_X,idf):
-#	indicepo = test_Y>0
+def predictor(classifier,test_X,test_Y,realtest_X,idf):
+#	indicepo = test_Y>00
 #	indicene = test_Y==0
 #	test_Y[indicepo]=1
 #	test_Y[indicene]=0
-	
-	loss=classifier.predict(test_X)
+	loss1=cross_val_predict(classifier,test_X,test_Y,cv=10)
+	loss=classifier.predict(realtest_X)
 	indicene = loss<1
 	loss[indicene]=0
-	test_X['loss'] = loss
-	test_X['id'] = idf
+	realtest_X['loss'] = loss
+	realtest_X['id'] = idf
 	#test_X = test_X.join(loss)
-	test_X[['id','loss']].to_csv("pred.csv",index=False)
+	realtest_X[['id','loss']].to_csv("pred.csv",index=False)
 	#f1=f1_score(test_Y, prediction)
-	np.savetxt("prediction.csv", prediction, delimiter=" ")
-	np.savetxt("test_y.csv", test_Y, delimiter=" ")
+	#np.savetxt("prediction.csv", prediction, delimiter=" ")
+	#np.savetxt("test_y.csv", test_Y, delimiter=" ")
 	#print ("accuracy", accuracy_score(test_Y, prediction))
 	#print ("f1 score", f1)
 	print ("MAE", mean_absolute_error(test_Y, prediction))
@@ -76,11 +90,11 @@ def predictor(classifier,test_X,idf):
 def main():
 	""" Combine functions to make predictions """
 	#load data
-	train_X, train_Y, test_X, idf= loadDat()
+	train_X, train_Y, test_X, test_Y, realtest_X,idf= loadDat()
 	#make default models
 	clf_model = train_model(train_X, train_Y)
 	#look at predictions
-	predictor(clf_model,test_X,idf)
+	predictor(clf_model,train_X,train_Y,realtest_X,idf)
 	#save prediction
 	#test[['id','loss']].to_csv("pred.csv",index=False)
 
