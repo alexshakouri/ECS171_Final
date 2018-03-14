30d914c2eacfeb600f28a7ddb71266e39d0d0181
bching28 <bryanching7373@gmail.com>
2018-03-13 20:55:42 -0700

    Update scilog_v2.py

--- scilog_v2.py
+++ scilog_v2.py
@@ -14,83 +14,23 @@ from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import cross_val_predict
 from sklearn.model_selection import KFold
 
-'''
-class PartialRegressor(object):
-
-    def __init__(self, clf):
-        self.clf = clf
-
-    def fit(self, X, y):
-        yy = y[y > 1]
-        XX = X[y > 0]
-        yy = np.log(yy)
-        temp = self.clf.fit(XX, yy)
-        for item in temp:
-            if temp[item].dtype not in ['float64']:
-                temp[item] = temp[item].astype(float)
-            #if test_X[item].dtype != train_X[item].dtype:
-            #    test_X[item] = test_X[item].astype(train_X[item].dtype)
-            if not all(temp[item].notnull()):
-                temp[item] = temp[item].fillna(temp[item].median())
-        return temp
-
-    def predict(self, X):
-        y = self.clf.predict(X)
-        y = np.exp(y)
-        y[y < 1] = 1.0
-        y[y > 100] = 100.0
-        return y
-'''
 def loadDat():
-    '''
-    train = pd.read_csv('data_train.csv')
-    test  = pd.read_csv('data_test.csv')
-    #featselect=['f2','f528_diff_f274','f332','f67','f25','f120','f766','f376','f39','f670','f228','f652','f415','f596','f406','f13','f355','f777','f2','f527_diff_f528','f528_diff_f274','f222','f332','f67','f25','f120','f766','f376','f670','f228','f652','f761','f4','f13','f386','f596','f9','f355','f406','f518','f328','f696','f674','f718'];
-    #featselect=list(set(featselect));
-    train['f527_diff_f528'] = train.f527 - train.f528
-    train['f528_diff_f274'] = train.f528 - train.f274
-    test['f527_diff_f528'] = train.f527 - train.f528
-    test['f528_diff_f274'] = train.f528 - train.f274
-    train_X = train#[featselect]
-    train_Y = train.loss
-    '''
     train_X = pd.read_csv('Train_X.csv')
     train_Y = pd.read_csv('Train_Y.csv')
     test_X = pd.read_csv('Test_X.csv')
     test_Y = pd.read_csv('Test_Y.csv')
-    '''
-    test_X  = train[35000:49999]#[featselect]
-    test_Y  = train.loss[35000:49999]
-    del train_X['loss']
-    del train_X['id']
-    del test_X['loss']
-    del test_X['id']
-    realtest_X=test;
-    for x in train_X:
-        if train_X[x].dtype not in ['float64']:
-            train_X[x] = train_X[x].astype(float)
-        if realtest_X[x].dtype not in ['float64']:
-            realtest_X[x] = realtest_X[x].astype(float)
-        if test_X[x].dtype != train_X[x].dtype:
-            test_X[x] = test_X[x].astype(train_X[x].dtype)
-        if realtest_X[x].dtype != train_X[x].dtype:
-            realtest_X[x] = realtest_X[x].astype(train_X[x].dtype)
-        if not all(train_X[x].notnull()):
-            train_X[x] = train_X[x].fillna(train_X[x].median())
-        if not all(test_X[x].notnull()):
-            test_X[x] = test_X[x].fillna(train_X[x].median())
-        if not all(realtest_X[x].notnull()):
-            realtest_X[x] = realtest_X[x].fillna(train_X[x].median())
-    '''
+    real_test_X = pd.read_csv('Real_Test_X.csv')
+    full_train_X = pd.read_csv('Full_Train_X.csv')
+    full_train_Y = pd.read_csv('Full_Train_Y.csv')
+
     idf = pd.read_csv('idf.csv')
-    #idf = realtest_X.id
-    #del realtest_X['id']
     scale = StandardScaler()
     train_X = pd.DataFrame(scale.fit_transform(train_X))
     test_X = pd.DataFrame(scale.fit_transform(test_X))
-    #realtest_X = pd.DataFrame(scale.fit_transform(realtest_X))
+    real_test_X = pd.DataFrame(scale.fit_transform(real_test_X))
+    full_train_X = pd.DataFrame(scale.fit_transform(full_train_X))
     #return train_X, train_Y, test_X, test_Y, realtest_X,idf
-    return train_X, train_Y, test_X, test_Y, idf
+    return train_X, train_Y, test_X, test_Y, real_test_X, full_train_X, full_train_Y, idf
 
 def train_model(train_X,train_Y):
 #	indicepo = train_Y>0
@@ -101,36 +41,29 @@ def train_model(train_X,train_Y):
     #classifier = GradientBoostingClassifier(n_estimators=65, learning_rate=0.3, max_depth=6, max_features='sqrt')
     classifier = GradientBoostingRegressor(loss='ls', n_estimators=500, learning_rate=0.0075, max_depth=10, max_features='sqrt', random_state=9753, verbose=0, min_samples_leaf=20, max_leaf_nodes=30, min_samples_split=20)
     classifier.fit(train_X, train_Y)
-    #clf = PartialRegressor(classifier)
-    #clf.fit(train_X, train_Y)
     return classifier
 
-def predictor(classifier, test_X, test_Y, idf):
+def predictor(classifier, test_X, test_Y, real_test_X, idf):
     cv = KFold(n_splits=5)
-    loss = cross_val_predict(classifier, test_X, test_Y, cv=cv)
-    #loss=classifier.predict(realtest_X)
-    #loss = clf.predict(test_X)
+    loss_cross = cross_val_predict(classifier, test_X, test_Y, cv=cv)
+    loss = classifier.predict(real_test_X)
     indicene = loss < 1
     loss[indicene] = 0
-    test_X['loss'] = loss
-    test_X['id'] = idf
-    #test_X = test_X.join(loss)
-    test_X[['id','loss']].to_csv("predictions.csv",index=False)
-    print "MAE", mean_absolute_error(test_Y, loss)
+    real_test_X['loss'] = loss
+    real_test_X['id'] = idf
+    real_test_X[['id','loss']].to_csv("predictions.csv",index=False)
+    print "MAE Loss Cross", mean_absolute_error(test_Y, loss)
+
 
 
 def main():
     """ Combine functions to make predictions """
     #load data
-    #train_X, train_Y, test_X, test_Y, realtest_X,idf= loadDat()
-    train_X, train_Y, test_X, test_Y, idf = loadDat()
+    train_X, train_Y, test_X, test_Y, real_test_X, full_train_X, full_train_Y, idf = loadDat()
     #make default models
-    clf_model = train_model(train_X, train_Y)
+    clf_model = train_model(full_train_X, full_train_Y)
     #look at predictions
-    predictor(clf_model, train_X, train_Y, idf)
-    #save prediction
-    #test[['id','loss']].to_csv("pred.csv",index=False)
-
+    predictor(clf_model, full_train_X, full_train_Y, real_test_X, idf)
 
 
 
